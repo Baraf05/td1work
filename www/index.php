e3d4bd086b9448131bdfa3ce2d9522d200b63b69
Baraf05 <barannfall@gmail.com>
2026-03-30 11:23:50 +0200

    kaka a jours2

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil - LocalHeb</title>
+    <title>Accueil - LocalHeb kaka a jours 2</title>
     <style>
         * {
             margin: 0;
